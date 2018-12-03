be5b7eec0088b107d7902da974aa078609c3fd2c
dmgig <dave.m.giglio@gmail.com>
2018-12-02 22:58:38 -0500

    fix reading of input

--- week1_programming_challenges/maximum.js
+++ week1_programming_challenges/maximum.js
@@ -1,9 +1,28 @@
 const args = process.argv.slice(3)
 
-const maximum = (args) => {
-  args.sort((a, b) => b - a)
-  const result = parseInt(args[0]) * parseInt(args[1])
-  return result
-}
 
-console.log(maximum(args))
+
+
+data = '';
+
+process.stdin.on('readable', function() {
+  var chunk;
+  while (chunk = process.stdin.read()) {
+    data += chunk;
+  }
+});
+
+process.stdin.on('end', function () {
+
+  let lines = data.split(/\r\n|\r|\n|\\n/)
+
+  let args = lines[1].split(' ')
+  const maximum = (args) => {
+    args.sort((a, b) => b - a)
+    const result = parseInt(args[0]) * parseInt(args[1])
+    return result
+  }
+
+  console.log(maximum(args))
+
+});
--- week1_programming_challenges/sum.js
+++ week1_programming_challenges/sum.js
@@ -1,5 +1,20 @@
-const args = process.argv.slice(2)
-const a = parseInt(args[0]);
-const b = parseInt(args[1]);
-const result = a + b;
-console.log(result)
+data = '';
+
+process.stdin.on('readable', function() {
+  var chunk;
+  while (chunk = process.stdin.read()) {
+    data += chunk;
+  }
+});
+
+process.stdin.on('end', function () {
+
+  let lines = data.split(/\r\n|\r|\n|\\n/)
+
+  const args = lines[0].split(' ')
+  const a = parseInt(args[0]);
+  const b = parseInt(args[1]);
+  const result = a + b;
+  console.log(result)
+
+});
