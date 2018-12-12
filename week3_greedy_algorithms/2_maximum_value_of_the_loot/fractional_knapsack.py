62112f6e3a7f91abb4f49b79d120735a21cdaf16
Giglio, Dave <dgiglio@penguinrandomhouse.com>
2018-12-12 18:07:21 -0500

    more greedy

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -3,42 +3,6 @@ import sys
 import random
 import math
 
-# def get_optimal_value(capacity, weights, values):
-#     curValue = 0.
-#     curWeight = 0
-#     sack = []
-#     # sort by values (reversed)
-#     values, weights = zip(*sorted(zip(values, weights), reverse=True))
-#     values = list(values)
-#     weights = list(weights)
-#     # sort by weights w/in values
-#     orderedValuesDict = {}
-#     for i in range(len(values)):
-#         if values[i] in orderedValuesDict:
-#             orderedValuesDict[values[i]].append(weights[i])
-#         else:
-#             orderedValuesDict[values[i]] = [weights[i]]
-#     for k in orderedValuesDict:
-#         # sort by weight
-#         sortedWeights = sorted(orderedValuesDict[k])
-#         # print('sortedWeights', sortedWeights)
-#         # fill sack
-#         for x in range(len(sortedWeights)):
-#             checkWeight = curWeight + sortedWeights[x]
-#             # check overcapacity and fill w/ fraction if over
-#             if(checkWeight > capacity):
-#                 spaceLeftOver = checkWeight - curWeight
-#                 pctToFill = spaceLeftOver / sortedWeights[x]
-#                 curValue = curValue + (k * pctToFill)
-#                 curWeight = curWeight + (sortedWeights[x] * pctToFill)
-#             else:
-#                 curValue = curValue + k
-#                 curWeight = curWeight + sortedWeights[x]
-#             # print('curValue, curWeight', curValue, curWeight)
-#
-#     return curValue
-
-
 def get_optimal_value(capacity, weights, values):
     curValue = 0.
     curWeight = 0
--- week3_greedy_algorithms/4_collecting_signatures/covering_segments.py
+++ week3_greedy_algorithms/4_collecting_signatures/covering_segments.py
@@ -1,22 +1,91 @@
 # Uses python3
 import sys
 from collections import namedtuple
+import operator
+import random
 
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
+
     points = []
-    #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+
+    if len(segments) == 0:
+        return points
+
+    # sort segments [a,b] by a
+    sortedSegments = sorted(segments, key=operator.itemgetter(0))
+
+    # print('init', sortedSegments)
+
+    while sortedSegments:
+
+        overlappingSegments = []
+        overlappingSegmentsTracker = []
+        # check all segments against segment 0 for overlap
+        # print('len(sortedSegments)', len(sortedSegments))
+        for j in range(1, len(sortedSegments)):
+            # print('check', sortedSegments[j][0], '<=', sortedSegments[0][1], '?')
+            if(sortedSegments[j][0] <= sortedSegments[0][1]):
+                # print(sortedSegments[j])
+                overlappingSegments.append(sortedSegments[j])
+                overlappingSegmentsTracker.append(j)
+                # print('len(overlappingSegments)', len(overlappingSegments))
+            else:
+                break
+
+        # print('overlap', overlappingSegments)
+        # print('len overlap', len(overlappingSegments))
+        # print('overlappingSegmentsTracker', overlappingSegmentsTracker)
+
+        # if no overlapping segments
+        if len(overlappingSegments) > 0:
+            possiblityA = sortedSegments[0][1]
+            del sortedSegments[0]
+            # append first point
+            possiblityB = sortedSegments[0][1]
+            points.append(min(possiblityA, possiblityB))
+            for k in overlappingSegmentsTracker:
+                del sortedSegments[k-1]
+            points = points + optimal_points(sortedSegments)
+            return points
+        else:
+            points.append(sortedSegments[0][1])
+            del sortedSegments[0]
     return points
 
 if __name__ == '__main__':
+
+    # stdin run
+    ###########
     input = sys.stdin.read()
     n, *data = map(int, input.split())
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+
     points = optimal_points(segments)
     print(len(points))
     for p in points:
         print(p, end=' ')
+
+    # stresstest run
+    ################
+    # while 1:
+    #
+    #     n = random.randint(1,100)
+    #     data = []
+    #     for i in range(n):
+    #         possibleStart = random.randint(0,10**9)
+    #         possibleEnd = random.randint(0,10**9)
+    #         data = data + [min(possibleStart, possibleEnd), max(possibleStart, possibleEnd)]
+    #
+    #     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+    #     # print('----')
+    #     # print('input', n, segments)
+    #     #
+    #     #
+    #     # print('----')
+    #     points = optimal_points(segments)
+    #     print(len(points))
+    #     for p in points:
+    #         print(p, end=' ')
+    #     print('----')
